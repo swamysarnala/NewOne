8088aa821d8781205c7caaa0543cc638c5f2588d
swamy sarnala <swamysarnala@gmail.com>
2017-06-20 19:16:53 -0700

    First Commit without Vinod Bayya

--- src/main/java/com/test/NewOne/App.java
+++ src/main/java/com/test/NewOne/App.java
@@ -8,7 +8,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("swamy sarnala");
+        int c[][][] = new int[4][4][4];
+       for(int i=0; i<4; i++){
+    	   for(int j=0; j<4; j++){
+    		   for(int k=0; k<4; k++){
+    			   c[i][j][k] = i+j+k;
+    		   }
+    	   }
+    	   
+       }
+       for(int i[][]: c){
+    	   for(int j[]: i){
+    		   for(int k: j){
+    			   System.out.print(" " + k);
+    		   }System.out.println(" ");
+    	   }
+    	   System.out.println(" ");
+       }
     }
 }
